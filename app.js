373ca8062b4cf866719476c80f75d0dd6f0e681e
Oksana3301 <dewiatika4295@gmail.com>
2021-03-01 21:57:48 +0700

    add function success, error and userlocation

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-function index(){ 
+function success(position){
+    console.log(position);
+}
+
+function error(){
+    alert('Maaf anda posisi tidak dapat kami lacak');
+}
+
+
+function userLocation() {
+
+    if(!navigator.geolocation){
+        alert('Geolocation tidak didukung oleh browser anda, silahkan gunakan browser lain');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+
     
+    
+    function index(){ 
+
     let app = document.getElementById('app');
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
 
     app.appendChild(h3);
+
+    userLocation();
 }
 
 index();
